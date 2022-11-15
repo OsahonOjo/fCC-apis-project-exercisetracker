0a61aef12c34924fc694d21a726f87f9b0946cfc
Replit user <>
2022-11-15 17:49:31 -0600

    commit

--- README.md
+++ README.md
@@ -1,3 +1,51 @@
-# Exercise Tracker
+# Exercise Tracker Full-Stack Web App
 
-This is the boilerplate for the Exercise Tracker project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/exercise-tracker
+### Project Description: 
+This web app is a certification project for freeCodeCamp's Backend Development and APIs course. This app allows you to create a new user and add exercise data for that user in a MongoDB database hosted on MongoDB Atlas. The app also allows you to display all exercise data logs for a given user.
+
+### Project Requirements:
+1. You can POST to "/api/users" with form data "username" to create a new user.
+2. The returned response from "POST /api/users" with form data "username" will be an object with "username" and "_id" properties.
+3. You can make a GET request to "/api/users" to get a list of all users.
+4. The GET request to "/api/users" returns an array.
+5. Each element in the array returned from "GET /api/users" is an object literal containing a user's "username" and "_id".
+6. You can POST to "/api/users/:_id/exercises" with form data "description", "duration", and optionally "date". If no "date" is supplied, the current date will be used.
+7. The response returned from "POST /api/users/:_id/exercises" will be the user object with the exercise fields added.
+8. You can make a GET request to "/api/users/:_id/logs" to retrieve a full exercise log of any user.
+9. A request to a user's log "GET /api/users/:_id/logs" returns a user object with a "count" property representing the number of exercises that belong to that user.
+10. A GET request to "/api/users/:_id/logs" will return the user object with a "log" array of all the exercises added.
+11. Each item in the "log" array that is returned from "GET /api/users/:_id/logs" is an object that should have "description", "duration", and "date" properties.
+12. The "description" property of any object in the "log" array that is returned from "GET /api/users/:_id/logs" should be a string.
+13. The "duration" property of any object in the "log" array that is returned from "GET /api/users/:_id/logs" should be a number.
+14. The "date" property of any object in the "log" array that is returned from "GET /api/users/:_id/logs" should be a string. Use the "dateString" format of the Date API.
+15. You can add "from", "to" and "limit" parameters to a "GET /api/users/:_id/logs" request to retrieve part of the log of any user. "from" and "to" are dates in "yyyy-mm-dd" format. "limit" is an integer of how many logs to send back.
+
+Your responses should have the following structures:<br>
+Exercise:<br>
+{<br>
+&nbsp;&nbsp; username: "fcc_test",<br>
+&nbsp;&nbsp; description: "test",<br>
+&nbsp;&nbsp; duration: 60,<br>
+&nbsp;&nbsp; date: "Mon Jan 01 1990",<br>
+&nbsp;&nbsp; _id: "5fb5853f734231456ccb3b05"<br>
+}
+
+User:<br>
+{<br>
+&nbsp;&nbsp; username: "fcc_test",<br>
+&nbsp;&nbsp; _id: "5fb5853f734231456ccb3b05"<br>
+}
+
+Log:<br>
+{<br>
+&nbsp;&nbsp; username: "fcc_test",<br>
+&nbsp;&nbsp; count: 1,<br>
+&nbsp;&nbsp; _id: "5fb5853f734231456ccb3b05",<br>
+&nbsp;&nbsp; log: [{<br>
+&nbsp;&nbsp;&nbsp;&nbsp; description: "test",<br>
+&nbsp;&nbsp;&nbsp;&nbsp; duration: 60,<br>
+&nbsp;&nbsp;&nbsp;&nbsp; date: "Mon Jan 01 1990",<br>
+&nbsp;&nbsp; }]<br>
+}
+
+These details can also be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/exercise-tracker
--- index.js
+++ index.js
@@ -1,18 +1,174 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config({path: './sample.env'});
+const cors = require('cors');
+const express = require('express');
+const urlParser = express.urlencoded({extended: true});
+const app = express();
 
 app.use(cors())
 app.use(express.static('public'))
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  count: { type: Number, default: 0 }, 
+  log: [{
+    description: String,
+    duration: Number,
+    date: String
+  }]
+});
+
+const User = mongoose.model('User', userSchema);
+
+function logError(operation, err) {
+  console.log(operation + " unsuccessful!");
+  console.log(err);
+}
+
+/* create a new user */
+app.post('/api/users', urlParser, function(req, res) {
+  /* MUST verify user-controlled properties in the req.body */
+  if (req.body.username) {    
+    let newUser = new User({
+      username: req.body.username,
+      log: []
+    });
+
+    newUser.save()
+           .then(function(document) {
+             res.json({ 
+               username: req.body.username, 
+               _id: document._id
+             });
+           })
+           .catch(function(err) {
+             logError('create_user', err);
+           });   
+  }
+});
+
+/* create a new exercise for an existing user */
+app.post('/api/users/:_id/exercises', urlParser, function(req, res) {
+  /* verify user-controlled input */
+  if (req.body.description && req.body.duration) {
+    let date = req.body.date ? new Date(req.body.date).toDateString() : new Date().toDateString();
+    
+    User.findById(req.params._id)
+        .then(document => {
+          /* update returned document */
+          document.log.push({
+            description: req.body.description,
+            duration: parseInt(req.body.duration),    /* duration is an integer */
+            date: date
+          });
+
+          /* save update to database */
+          document.save()
+                  .then(document => {
+                    let response = {
+                      _id: document._id.toString(),
+                      username: document.username,
+                      date: date.toString(),
+                      duration: parseInt(req.body.duration),
+                      description: req.body.description
+                    };
+                    res.json(response);
+                  })
+                  .catch(err => {
+                    logError('add_exercise', err);
+                    res.send("Operation failed.");
+                  });                   
+        })
+        .catch(err => {
+          logError('add_exercise', err);
+          res.send("Operation failed.");
+        });
+  }
+});
+
+/* GET all exercise data logs for a given user, 
+ * with options for logs within a range of dates and limiting number of logs returned
+ */
+const getLogs = function(document, options) {
+  let response = {
+    username: document.username,
+    _id: document._id.toString(),
+    count: document.log.length,
+    log: []
+  };
+
+  /* ensure correct data types */
+  for (let i = 0; i < document.log.length; i++) {
+    response.log.push({ 
+      description: document.log[i].description,
+      duration: parseInt(document.log[i].duration), 
+      date: document.log[i].date.toString()
+    });
+  }
+
+  if (options) {
+    let fromString = new Date(options.from).toDateString();
+    let toString = new Date(options.to).toDateString();
+  
+    /* convert date to milliseconds */
+    let fromMS = Date.parse(fromString);    
+    let toMS = Date.parse(toString);
+  
+    let limit = parseInt(options.limit);
+
+    let tempLog = [...response.log];
+    tempLog.filter(log => (Date.parse(log.date) >= fromMS && Date.parse(log.date) <= toMS));
+    
+    /* limit the number of responses */
+    if (response.log.length > limit) {
+      response.log.splice(limit);
+      response.count = response.log.length;
+    }
+  }
+  
+  return response;
+};
+
+app.get('/api/users/:_id/logs', function(req, res) {
+  const { from, to, limit } = req.query;
+  
+  if (req.params._id) {    
+    User.findById(req.params._id)
+        .then(document => {
+          let response = getLogs(document, { from: from, to: to, limit: limit });
+          res.json(response);
+        })
+        .catch(err => {
+          logError('get_all_user_logs', err);
+        });
+  }
+});
+
+/* return a list of all users */
+app.get('/api/users', function(req, res) {
+  User.find({})    /* return all documents */
+      .then(arrayofDocuments => {
+        let response = [];
+        for (let i = 0; i < arrayofDocuments.length; i++) {
+          response.push({ _id: arrayofDocuments[i]._id, username: arrayofDocuments[i].username });
+        }
+        res.send(response);
+      })
+      .catch(err => {
+        logError('get_all_users', err);
+      });  
+});
+
+app.get('/mongo-health', function(req, res) {
+  res.json({ status: mongoose.connection.readyState });
+});
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
-})
+});
